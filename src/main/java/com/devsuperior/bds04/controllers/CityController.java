d6735e5d1d708b2a473d9e5efcd9cf87b56aecff
Rafael de Luca <deluca1712@gmail.com>
2022-09-16 15:29:15 -0300

    Método insert new City

--- src/main/java/com/devsuperior/bds04/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.bds04.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds04.dto.CityDTO;
 import com.devsuperior.bds04.services.CityService;
@@ -24,5 +28,15 @@ public class CityController {
 		List<CityDTO> listOfCity = service.findAll();
 		return ResponseEntity.ok().body(listOfCity);
 	}
+	
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
+	}	
 
 }
