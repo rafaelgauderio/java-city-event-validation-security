86163820e4191e08546c1650d70dd37ec14b0e6b
Rafael De Luca <deluca1712@gmail.com>
2022-09-17 15:48:46 -0300

    Classe AuthorizationServerConfigurerAdapter

--- src/main/java/com/devsuperior/bds04/config/WebSecurityConfig.java
+++ src/main/java/com/devsuperior/bds04/config/WebSecurityConfig.java
@@ -1,18 +1,49 @@
 package com.devsuperior.bds04.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private BCryptPasswordEncoder codificadorDeSenha;
+	
+	@Autowired
+	private UserDetailsService servicoDetalhesUsuario;
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/actuator//**");
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(servicoDetalhesUsuario).passwordEncoder(codificadorDeSenha);
+		
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		
+		return super.authenticationManager();
+	}
+
+
+	
+	
+	
 	
 	
 	
